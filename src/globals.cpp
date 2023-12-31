00951af37a9b2da4279cda6eb93808cb2ff4c031
AgilanSam <138312709+AgilanSam@users.noreply.github.com>
2023-12-30 22:54:30 -0800

    Update globals.cpp

--- src/globals.cpp
+++ src/globals.cpp
@@ -1,13 +1,16 @@
 #include "globals.hpp"
 
+//inertial sensor
 pros::IMU inertial(12);
 
+//left motors
 pros::MotorGroup leftMotors(std::initializer_list<pros::Motor> {
     pros::Motor(-15, pros::E_MOTOR_GEAR_BLUE), // left front motor. port 8, reversed
     pros::Motor(-11, pros::E_MOTOR_GEAR_BLUE), // left middle motor. port 20, reversed
     pros::Motor(-14, pros::E_MOTOR_GEAR_BLUE),
 });
 
+//right motors
 pros::MotorGroup rightMotors(std::initializer_list<pros::Motor> {
     pros::Motor(16, pros::E_MOTOR_GEAR_BLUE),
     pros::Motor(17, pros::E_MOTOR_GEAR_BLUE),
@@ -15,21 +18,26 @@ pros::MotorGroup rightMotors(std::initializer_list<pros::Motor> {
 });
 
 
-// left motors, right motors, inertial, wheelDiameter, gear Ratio, lateralPID, turnPID, swingPID
-Chassis chassis(
-    &leftMotors, &rightMotors, &inertial, 
-    2.75, 1, 
-    //lateral
-    PID(10, 0, 45, 0, 0.75, 250, 2, 750, 7000),
-    PID(10, 0, 45, 0, 0.75, 250, 1, 750, 7000),
-    //angular 
-    PID(3.5, 0, 15, 0, 1, 100, 3, 250, 1000),
 
+Chassis chassis(
+    //left motors, right motors, inertial sensor
+    &leftMotors, &rightMotors, &inertial, 
+    //wheel diameter, gear ratio
+    2.75, 1, 
+    //forward PID
+    PID(10, 0, 45, 0, 0.75, 250, 2, 750, 7000),
+    //backward PID
+    PID(10, 0, 45, 0, 0.75, 250, 1, 750, 7000),
+    //turnPID
     PID(3.5, 0, 15, 0, 1, 100, 3, 250, 1000),
+    //swing PID
+    PID(3.5, 0, 15, 0, 1, 100, 3, 250, 1000),
+    //arc PID
     PID(6, 0, 50, 0, 1, 100, 3, 250, 4000),
+    //heading PID
     PID(5, 0, 15, 0, 1, 100, 3, 250, 1000)
 );
-
+//that's all it takes for integrated encoder odom!
 
 
 pros::Controller master(pros::E_CONTROLLER_MASTER);
