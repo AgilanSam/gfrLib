b507b6454f73b40463298703980c0e723d0a4605
Arpit Jena <99858283+arpitjena1@users.noreply.github.com>
2024-01-03 00:15:39 -0800

    Update chassis.hpp

--- include/chassis.hpp
+++ include/chassis.hpp
@@ -45,8 +45,8 @@ class Chassis {
     
         void moveToPose(float x1, float y1, float theta1, int timeout, bool forwards, float maxSpeed, bool async,float chasePower,
                           float lead, float smoothness, bool linearexit, float linearexitrange);
-        double x;
-        double y;
+        double x = 0;
+        double y = 0;
         double heading = 0;
         double heading_radians;
         float currentPose[3];
