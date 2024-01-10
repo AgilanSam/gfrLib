3f36a2e6b4db9ae8204266ed0c1dd7f1fee191bc
Arpit Jena <99858283+arpitjena1@users.noreply.github.com>
2024-01-10 09:49:13 -0800

    Update chassis.hpp

--- include/chassis.hpp
+++ include/chassis.hpp
@@ -21,41 +21,41 @@ class Chassis {
                 const float wheelDiameter, const float trackWidth, const float gearRatio, PID drivePID, PID backwardPID, PID turnPID, PID swingPID, PID arcPID, PID headingPID);
 
         void calibrate();
-
+        void setPose(float x1, float y1, float theta1);
+        std::pair<double, double> getPose();
+        
         void setHeading(float heading);
-
         void tank(float left, float right);
         void arcade(float lateral, float angular);
     
         void move(float distance, float maxSpeed=127);
         void turn(float heading, float maxSpeed=127);
         void swing(float heading, bool isLeft, float maxSpeed=127);
-        void turnToPoint(float x1, float y1, int timeout, float maxSpeed=127);
+        void arc(float heading, double leftMult, double rightMult);
         void move_without_settle(float distance, float exitrange);
         void swing_without_settle(float heading, bool isLeft, float timeout);
-        void moveToPoint(float x1, float y1, int timeout, float maxSpeed=127);
-        void moveToPointTurn(float x1, float y1, int timeout, float maxSpeed=127);
-        void arc(float heading, double leftMult, double rightMult);
-        void setPose(float x1, float y1, float theta1);
-        void activeMoveToPoint(float x1, float y1, int timeout, float maxSpeed=127);
+        
+        
+       
+        
+        void moveToPoint(float x1, float y1, int timeout, float maxSpeed = 127);
+        void turnToPoint(float x1, float y1, int timeout, float maxSpeed=127);
         void move_without_settletime(float distance, float timeout); 
         void waitUntilDist(float dist);
-        std::pair<double, double> getPose();
-        void moveChassis(float linearVelocity, float angularVelocity);
-    
         void moveToPose(float x1, float y1, float theta1, int timeout, bool forwards, float maxSpeed, bool async,float chasePower,
                           float lead, float smoothness, bool linearexit, float linearexitrange);
+
         double x = 0;
         double y = 0;
         double heading = 0;
-        double heading_radians;
-        float currentPose[3];
-        void ramsete(Pose targetPose, Pose currentPose, float targetAngularVelocity, float targetLinearVelocity, float beta, float zeta);
-        int FindClosest(Pose pose, std::vector<Pose> pPath, int prevCloseIndex=0);
+        
+        
         void followPath(std::vector<Pose> pPath, float targetLinVel, float targetAngVel, float timeOut, float errorRange, float beta, float zeta, bool reversed);
         
     private:
-        
+        void moveChassis(float linearVelocity, float angularVelocity);
+        void ramsete(Pose targetPose, Pose currentPose, float targetAngularVelocity, float targetLinearVelocity, float beta, float zeta);
+        int FindClosest(Pose pose, std::vector<Pose> pPath, int prevCloseIndex=0);
         pros::MotorGroup* leftMotors;
         pros::MotorGroup* rightMotors;
         pros::IMU* imu;
